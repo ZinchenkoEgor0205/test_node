b5fd1d52c39a8478e106b6288e8d708933201099
Egor Zinchenko <zinchenko02051999@gmail.com>
2024-09-04 15:56:42 +0300

    update

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 const express = require('express');
+const winston = require('winston');
+const cors = require('cors')
 const { swaggerUi, swaggerSpec } = require('./swagger');
 const loginRouter = require('./login');
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+app.use(cors());
 
-let books = [
-  { id: 1, title: '1984', author: 'George Orwell' },
-  { id: 2, title: 'The Alchemist', author: 'Paulo Coelho' },
-  { id: 3, title: 'The Da Vinci Code', author: 'Dan Brown' },
-];
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    logger.info({
+      method: req.method,
+      url: req.url,
+      status: res.statusCode,
+      duration: `${duration}ms`,
+      requestBody: req.body,
+      responseBody: res.locals.responseData // Assuming you've saved the response data in res.locals
+    });
+  });
+  next();
+});
+
+
+app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/login', loginRouter);
