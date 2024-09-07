da9fd15c932c611ee62612426b6556a9ee3510b3
Egor <zinchenko02051999@gmail.com>
2024-09-07 21:47:32 +0300

    update

--- webHook.js
+++ webHook.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const {log} = require("winston");
 const router = express.Router();
 
 
 router.get('/', (req, res) => {
-
+    console.log(req.query['hub.challenge']);
     console.log(req.query['hub.challenge'])
-    res.json(req.query['hub.challenge']);
+    res.send(req.query['hub.challenge'])
 });
 
 module.exports = router
